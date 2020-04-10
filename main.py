ac3f2dd8fd685528f881f99bcea0a2b441ad6678
Mateusz Chiliński <mateusz@chilinski.eu>
2020-04-10 09:28:00 +0200

    some minor preparation changes, added vcf files to use for development and an example later on

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import argparse
+import shlex
+from subprocess import Popen, PIPE
 
 parser = argparse.ArgumentParser(description='Gets the SV consensus.')
 parser.add_argument('sv_folder', metavar='sv_folder',
@@ -9,3 +11,13 @@ parser.add_argument('sv_folder', metavar='sv_folder',
 
 args = parser.parse_args()
 print(args.sv_folder)
+
+# preprocessing of the files
+# problems with no svlen?
+# bcftools query -i '(SVLEN < 50000 && SVLEN > 50) || (SVLEN > -50000 && SVLEN < -50)' -f '%CHROM\t%POS\t%ID\t%REF\t%FIRST_ALT\t%QUAL\t%FILTER\tEND=%END;SVLEN=%SVLEN;SVTYPE=%SVTYPE;CIPOS=%CIPOS;CIEND=%CIEND\tGT\t[ %GT]\n' Manta.vcf > MantaxD.vcf
+# >50bp, <50?kbp
+#cmd = ""
+#process = Popen(shlex.split(cmd), stdout=PIPE)
+#process.communicate()
+#exit_code = process.wait()
+
