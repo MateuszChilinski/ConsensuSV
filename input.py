c4f6d35962f7dc524321176351574f5c7e363af5
Mateusz Chiliński <mateusz@chilinski.eu>
2020-04-14 21:53:17 +0200

    qf

---
+++ input.py
@@ -0,0 +1,17 @@
+def inputHandling():
+    parser = argparse.ArgumentParser(description='Gets the SV consensus.')
+    parser.add_argument('-f', '--sv_folder', help='Folder containing raw outputs from SV callers.', required=True)
+
+    parser.add_argument('-s', '--sample', help='Name of the sample.', required=True)
+
+    parser.add_argument('-o', '--output', help='Output file.', default="output.vcf")
+
+    parser.add_argument('-m', '--min_overlap', help='Minimum number of SVs in the neighbourhood for the SV to be reported (default 3).', type=int, default=3)
+
+    parser.add_argument('-t', '--truth', help='File used for training new model.', required=False)
+
+    parser.add_argument('-np', '--no_preprocess', help='Flag used for skipping the preprocessing process - all the preprocessed files should be in temp/ folder.', action="store_true", required=False)
+
+    args = parser.parse_args()
+
+    return args
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import numpy
 import sys
 import pickle
-import Utilities
+import utilities
 import SVTool
 import shutil
 
@@ -12,21 +12,10 @@ from os import listdir
 from os.path import isfile, join
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
+from input import inputHandling
 
 
-parser = argparse.ArgumentParser(description='Gets the SV consensus.')
-parser.add_argument('-f', '--sv_folder', help='Folder containing raw outputs from SV callers.', required=True)
-parser.add_argument('-s' '--sample', help='Name of the sample.', required=True)
-
-parser.add_argument('-o', '--output', help='Output file.', default="output.vcf")
-
-parser.add_argument('-m', '--min_overlap', help='Minimum number of SVs in the neighbourhood for the SV to be reported (default 3).', type=int, default=3)
-
-parser.add_argument('-t', '--truth', help='File used for training new model.', required=False)
-
-parser.add_argument('-np', '--no_preprocess', help='Flag used for skipping the preprocessing process - all the preprocessed files should be in temp/ folder.', action="store_true", required=False)
-
-args = parser.parse_args()
+inputHandling()
 
 sv_files = [f for f in listdir(args.sv_folder) if isfile(join(args.sv_folder, f))]
 
