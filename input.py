8df2c6dc666afbf433e8da01ef14f4b4bc396a35
Mateusz Chiliński <mateusz@chilinski.eu>
2020-04-16 12:24:33 +0200

    multiple samples preprocessing

--- input.py
+++ input.py
@@ -2,19 +2,18 @@ import argparse
 
 def inputHandling():
     parser = argparse.ArgumentParser(description='Gets the SV consensus.')
-    parser.add_argument('-f', '--sv_folder', help='Folder containing raw outputs from SV callers.', required=True)
 
-    parser.add_argument('-s', '--sample', help='Name of the sample.', required=True)
+    parser.add_argument('-f', '--sv_folder', help='Folder containing folders of samples with raw outputs from SV callers (comma-separated). More information on the structure of the samples folder in readme.', required=True)
 
     parser.add_argument('-mod', '--model', help='Model used for SV discovery (default default.model).', required=False, default="default.model")
 
-    parser.add_argument('-o', '--output', help='Output file.', default="output.vcf")
+    parser.add_argument('-o', '--output', help='Output file prefix.', default="consensuSV_")
 
     parser.add_argument('-m', '--min_overlap', help='Minimum number of SVs in the neighbourhood for the SV to be reported (default 3).', type=int, default=3)
 
-    parser.add_argument('-t', '--truth', help='File used for training new model. It is preprocessed even if --no_preprocess flag is set.', required=False)
+    parser.add_argument('-t', '--train', help='Creates new model. Requires truth.vcf to be present in all the sv folders. VCF file truth.vcf is preprocessed even if flag --no_preprocess is set.', action="store_true", required=False)
 
-    parser.add_argument('-np', '--no_preprocess', help='Flag used for skipping the preprocessing process - all the preprocessed files should be in temp/ folder.', action="store_true", required=False)
+    parser.add_argument('-np', '--no_preprocess', help='Flag used for skipping the preprocessing process - all the preprocessed files should be in temp/ folder.', required=False)
 
     args = parser.parse_args()
 
--- main.py
+++ main.py
@@ -11,85 +11,100 @@ from sklearn.neural_network import MLPRegressor
 from input import inputHandling
 from SVTools import SVariant
 from shutil import copyfile
+from os.path import isdir, join
 
 args = inputHandling()
-
-print(args.truth)
 
 
 
 # preprocessing of the files
 # problems with no svlen?
-if not (args.no_preprocess):
-    shutil.rmtree("temp");
-    os.mkdir("temp");
-    if (args.truth is not None):
-        copyfile(args.truth, "temp/truth.vcf")
-    print("Preprocessing files...")
-
-    sv_tools = utilities.preprocessFiles(args.sv_folder, args.sample)
-else:
-    if (args.truth is not None):
-        copyfile(args.truth, "temp/truth.vcf")
-        utilities.preprocessFile("truth.vcf", utilities.generate_header(args.sample))
-    sv_tools = utilities.loadTempFiles(args.sample)
-percDiff = 0.1
 
 X_vector = list()
 Y_vector = list()
 
-resulting_svs = list()
+samples_folder = args.sv_folder
+folders = [f for f in listdir(samples_folder) if isdir(join(samples_folder, f))]
 
-consensusId = 1
+samples = [f.split('/')[-1] for f in folders]
 
-if (args.truth is None): # load model
-    filename = 'pretrained.model'
-    loaded_model = pickle.load(open(filename, 'rb'))
-i = 0
-for svtool in sv_tools:
-    if (args.truth is not None):
-        if(svtool.tool != "truth"):
-            continue
-    for sv in svtool.sv_list:
-        if(sv.used): continue
-        candidates = list()
-        candidates.append(sv)
-        for svtool2 in sv_tools:
-            if(svtool.tool == svtool2.tool):
+for sample in samples:
+    print(sample)
+    if not (args.no_preprocess):
+        shutil.rmtree("temp");
+        os.mkdir("temp");
+
+        print("Preprocessing files of "+sample+"...")
+
+        sv_tools = utilities.preprocessFiles(args.sv_folder, args.sample)
+    else:
+        if (args.train is not None):
+            utilities.preprocessFile("truth.vcf", utilities.generate_header(args.sample))
+        sv_tools = utilities.loadTempFiles(args.sample)
+    percDiff = 0.1
+
+    consensusId = 1
+
+    if (args.truth is None): # load model
+        filename = 'pretrained.model'
+        loaded_model = pickle.load(open(filename, 'rb'))
+
+    resulting_svs = list()
+    for svtool in sv_tools:
+        if (args.truth is not None):
+            if(svtool.tool != "truth"):
                 continue
-            for sv2 in svtool2.sv_list:
-                if(sv.chrom != sv2.chrom):
+        for sv in svtool.sv_list:
+            if(sv.used): continue
+            candidates = list()
+            candidates.append(sv)
+            for svtool2 in sv_tools:
+                if(svtool.tool == svtool2.tool):
                     continue
-                if(sv2.pos > sv.pos+500): # fix later! it should be dependend on ci or % of svlen
-                    break
-                if(sv2.used): continue
-                if(sv.checkOverlap(sv2)):
-                   candidates.append(sv2)
-                   break
-        if(len(candidates) < 3): # if fewer than 3 then no point in checking it out
-            continue
-        if (args.truth is not None): # learning phase
-            candidates.remove(sv)
-            X_vector.append(candidates)
-            Y_vector.append(sv)
-        else:
-            freqDict = utilities.buildFreqDict(candidates)
-
-            # maybe remove all candidates from svtool once consensus was established based on it?
-            (majorityFound, firstMajor) = utilities.findMajority(sv, freqDict, candidates)
-            if(majorityFound):
-                newSv = SVariant("consensus", None, firstMajor.chrom, firstMajor.pos, "consensus_"+str(consensusId), firstMajor.ref, firstMajor.end, firstMajor.gt, firstMajor.svlen, firstMajor.svtype, 
-                                 -10, 10, -10, 10, utilities.generateAlgorithmsList(candidates))
-                consensusId += 1
+                for sv2 in svtool2.sv_list:
+                    if(sv.chrom != sv2.chrom):
+                        continue
+                    if(sv2.pos > sv.pos+500): # fix later! it should be dependend on ci or % of svlen
+                        break
+                    if(sv2.used): continue
+                    if(sv.checkOverlap(sv2)):
+                       candidates.append(sv2)
+                       break
+            if(len(candidates) < 3): # if fewer than 3 then no point in checking it out
+                continue
+            if (args.truth is not None): # learning phase
+                candidates.remove(sv)
+                X_vector.append(candidates)
+                Y_vector.append(sv)
             else:
-                #print("1")
-                result = loaded_model.predict(utilities.preprocess_X([candidates]))
-                pos = result[0]
-                end = result[1]
-                newSv = SVariant("consensus", None, sv.chrom, int(round(pos)), "consensus_"+str(consensusId), sv.ref, int(round(end)), sv.gt, int(round(pos-end)), sv.svtype, -10, 10, -10, 10, utilities.generateAlgorithmsList(candidates))
-                consensusId += 1
-            resulting_svs.append(newSv)
-            utilities.markUsedCandidates(candidates)
+                freqDict = utilities.buildFreqDict(candidates)
+
+                # maybe remove all candidates from svtool once consensus was established based on it?
+                (majorityFound, firstMajor) = utilities.findMajority(sv, freqDict, candidates)
+                if(majorityFound):
+                    newSv = SVariant("consensus", None, firstMajor.chrom, firstMajor.pos, "consensus_"+str(consensusId), firstMajor.ref, firstMajor.end, firstMajor.gt, firstMajor.svlen, firstMajor.svtype, 
+                                     -10, 10, -10, 10, utilities.generateAlgorithmsList(candidates))
+                    consensusId += 1
+                else:
+                    #print("1")
+                    result = loaded_model.predict(utilities.preprocess_X([candidates]))
+                    pos = result[0]
+                    end = result[1]
+                    newSv = SVariant("consensus", None, sv.chrom, int(round(pos)), "consensus_"+str(consensusId), sv.ref, int(round(end)), sv.gt, int(round(pos-end)), sv.svtype, -10, 10, -10, 10, utilities.generateAlgorithmsList(candidates))
+                    consensusId += 1
+                resulting_svs.append(newSv)
+                utilities.markUsedCandidates(candidates)
+    if (args.truth is None):
+        header = utilities.generate_header(args.sample)
+        with open("output.vcf", 'w') as fout:
+            fout.write(header)
+            for sv in resulting_svs:
+                fout.write(sv.printVcfLine())
+
+        cmd = "cat output.vcf | awk '$1 ~ /^#/ {print $0;next} {print $0 | "+ "\"sort -k1,1V -k2,2n\"" + r"}' > output_sorted.vcf"
+        utilities.execute_command(cmd)
+
+        os.replace("output_sorted.vcf", args.output)
 
 if (args.truth is not None): # learning phase
     X_preprocessed_vector = utilities.preprocess_X(X_vector)
@@ -114,17 +129,7 @@ if (args.truth is not None): # learning phase
     pickle.dump(nn, open(filename, 'wb'))
 
     os.remove("temp/truth.vcf")
-else:
-    header = utilities.generate_header(args.sample)
-    with open("output.vcf", 'w') as fout:
-        fout.write(header)
-        for sv in resulting_svs:
-            fout.write(sv.printVcfLine())
 
-    cmd = "cat output.vcf | awk '$1 ~ /^#/ {print $0;next} {print $0 | "+ "\"sort -k1,1V -k2,2n\"" + r"}' > output_sorted.vcf"
-    utilities.execute_command(cmd)
-
-    os.replace("output_sorted.vcf", args.output)
 
 #numpy.savetxt("foo.csv", numpy.concatenate((X_test, numpy.vstack((y_test,y_pred)).T), axis=1), delimiter=',', comments="")
 
