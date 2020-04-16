17c9752c7a5a85e93b7b22ffca67693501f6fb30
Mateusz Chiliński <mateusz@chilinski.eu>
2020-04-16 14:45:36 +0200

    qf

--- main.py
+++ main.py
@@ -125,7 +125,7 @@ if (args.train): # learning phase
 
     X_preprocessed_vector = utilities.preprocess_X(X_vector)
     Y_preprocessed_vector = utilities.preprocess_Y(Y_vector)
-
+    print(len(X_preprocessed_vector))
     X_train, X_test, y_train, y_test = train_test_split(X_preprocessed_vector, Y_preprocessed_vector, test_size=0.1, random_state=42, shuffle=True)
     nn = MLPRegressor(hidden_layer_sizes=(49, 14, 7), solver='lbfgs', max_iter=int(1e3), max_fun=15000, random_state=0)
 
