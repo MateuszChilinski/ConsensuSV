e49ef3603a7e228b1198f9a40f47a02302daad6d
Mateusz Chiliński <mateusz@chilinski.eu>
2020-04-16 19:39:48 +0200

    qf

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ if (args.train): # learning phase
     preprocessing.scale(X_preprocessed_vector)
 
     X_train, X_test, y_train, y_test = train_test_split(X_preprocessed_vector, Y_preprocessed_vector, test_size=0.1, random_state=42, shuffle=True)
-    nn = MLPRegressor(hidden_layer_sizes=(14, 7), solver='adam', random_state=0)
+    nn = MLPRegressor(hidden_layer_sizes=(14, 7), solver='lbfgs', random_state=0)
 
     print("Creating the model...")
 
